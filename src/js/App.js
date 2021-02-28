0257af3a110272a89c9c91d09c53f481ef2e6466
pedrohb29 <pedrobotecchi@gmail.com>
2021-02-28 18:14:58 -0300

    Linking Electron with React

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+    console.log('Testing')
+    return (
+        <>
+        <h1>
+            I'm an app component    
+        </h1>
+        <button>Notify</button>
+        </>
+    )
+}
